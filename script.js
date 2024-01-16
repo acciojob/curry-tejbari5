0a1ca3f4bfd205de047356749c61056efdea6517
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-16 10:06:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function curry(callback) {
-  
+  const arity = callback.length;
+
+  return function curried(...args) {
+    if (args.length >= arity) {
+      return callback(...args);
+    } else {
+      return function(...moreArgs) {
+        return curried(...args, ...moreArgs);
+      };
+    }
+  };
 }
 
-module.export=curry
+module.exports = curry;
